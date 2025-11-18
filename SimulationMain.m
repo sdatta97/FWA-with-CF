551faeeeab2c194b1db7a4670ef4f313ca4e32bd
sdatta97 <sdatta@nyu.edu>
2025-11-18 15:27:23 -0500

    add changes and bug fixes to utility computations

--- SimulationMain.m
+++ SimulationMain.m
@@ -34,8 +34,8 @@ params.n_samples = floor(params.coherence_time/params.Ts);
 % params.mob_rho = (besselj(0,2*pi*params.Ts*params.ue_velocity*params.fc/params.c))^params.n_samples;
 params.mob_rho = besselj(0,2*pi*params.Ts*params.ue_velocity*params.fc/params.c);
 %% Economics params
-params.price_cell = 20;
-params.price_FWA = 90;
+params.price_cell_per_bit = 4;
+params.price_FWA_per_bit = 0.14;
 params.r_min_cell = 10e6;
 params.r_min_FWA = 50e6;
 params.r_max_FWA = 50e6;
@@ -56,7 +56,7 @@ params.Kt_Kr_vsUE  = 0; %0.175^2; %0.175^2; %[1,2,3,4];  %to save 1=AP 0.1,UE=0.
 params.pilot_pow = 100;  % 0.1W   % UL pilot. power (W)
 params.noiseFigure = 9; % gue
 params.sigma_sf =4;
-params.Band = 100e6; %Communication bandwidth
+params.Band = 400e6; %Communication bandwidth
 
 params.SC = 0; %0 - CF, 1 - SC
 %% Define simulation setup
@@ -69,16 +69,15 @@ params.ASD_theta = 0; %deg2rad(15);  %elevation angle
 params.p = 100;
 
 %Power factor division
-p_fac_arr = 10;%[1, 10:10:30]; %[1 20:10:100];
+p_fac_arr = [1, 5:5:50];
 numCPE_all = 50; %5:5:20;
 
 %Prepare to save simulation results
 %% Room Setup, UE placement, UE height
 params.deployRange = 200; %20:20:100;
-params.deployRange_sub6 = 1000;
 params.coverageRange_sub6 = 430;
-params.num_antennas_per_gNB = 64;
-params.num_antennas_per_sc = 16;
+params.num_antennas_per_gNB = 256;
+params.num_antennas_per_sc = 64;
 params.rho_tot = 10^(3.6)*params.num_antennas_per_gNB; %200;
 params.rho_tot_sc = 10^(3.6)*params.num_antennas_per_sc;
 %Number of antennas per UE
@@ -87,10 +86,10 @@ params.N_UE_cell = 1; %4;
 params.hr = 1;
 params.ht_bs = 15;
 params.ht_sc = 5;
-lambda_BS = 20; %([5 6 7 8 9 10]).^2;
-lambda_UE = 100; %250:250:1000; %200:10:250; %150; %100:50:200; %[30:20:90, 100]; %100;
+lambda_BS = 5; %([5 6 7 8 9 10]).^2;
+lambda_SC = 20; %([5 6 7 8 9 10]).^2;
+lambda_UE = 200; %250:250:1000; %200:10:250; %150; %100:50:200; %[30:20:90, 100]; %100;
 params.Lmax = 3;
-lb_thresh = 0; %0.1; %[0:0.05:0.1 0.5 1];
 params.preLogFactor = 1;
 %Number of channel realizations per setup
 params.nbrOfRealizations = 1;
@@ -100,13 +99,12 @@ lookAngleCell{1} = [0,360];
 %% Simulation FR1 setup
 for idxUEDensity = 1:length(lambda_UE)
     %% UE locations
-    params.numUE = ceil(lambda_UE(idxUEDensity)*pi*(params.deployRange_sub6/1000)^2);
-    RUE = params.deployRange_sub6*sqrt(rand(params.numUE,1)); %location of UEs (distance from origin)
+    params.numUE = ceil(lambda_UE(idxUEDensity)*pi*(params.deployRange/1000)^2);
+    RUE = params.deployRange*sqrt(rand(params.numUE,1)); %location of UEs (distance from origin)
     angleUE = 2*pi*rand(params.numUE,1);%location of UEs (angle from x-axis)
     params.UE_locations =  [RUE.*cos(angleUE), RUE.*sin(angleUE)];   
     %% CPE locations
     deployRange = params.deployRange; 
-    deployRange_sub6 = params.deployRange_sub6; 
     RCPE =  deployRange*sqrt(rand(numCPE_all,1)); %location of UEs (distance from origin)
     angleCPE = 2*pi*rand(numCPE_all,1);%location of UEs (angle from x-axis)
     CPE_locations = [RCPE.*cos(angleCPE), RCPE.*sin(angleCPE)];
@@ -116,14 +114,15 @@ for idxUEDensity = 1:length(lambda_UE)
         params.RgNB = params.deployRange*sqrt(rand(params.numGNB,1));
         params.angleGNB = 2*pi*rand(params.numGNB,1);
         params.locationsBS = [params.RgNB.*cos(params.angleGNB), params.RgNB.*sin(params.angleGNB)];
-        params.numSC = ceil(lambda_BS(idxBSDensity)*pi*((params.deployRange_sub6/1000)^2-(params.deployRange/1000)^2));
-        params.Rsc = params.deployRange_sub6*sqrt(rand(params.numSC,1));
+        params.numSC = ceil(lambda_SC(idxBSDensity)*pi*(params.deployRange/1000)^2);
+        params.Rsc = params.deployRange*sqrt(rand(params.numSC,1));
         params.anglesc = 2*pi*rand(params.numSC,1);
         params.locationsSC = [params.Rsc.*cos(params.anglesc), params.Rsc.*sin(params.anglesc)];
         params.numCPE = numCPE_all;
         params.CPE_locations = CPE_locations;
         [gainOverNoisedB,R_gNB,R_cpe,R_ue,pilotIndex,D,D_small,APpositions,UEpositions,distances] = generateSetup(params,str2double(aID));
         [gainOverNoisedB_sc,R_sc,R_cpe_sc,R_ue_sc,D_sc,D_small_sc,SCpositions,distances_sc] = generateSetup_SC(params,str2double(aID),pilotIndex,D,D_small,UEpositions);
+        max_obj = 0;
         max_cell_util = 0;
         max_FWA_util = 0;
         K_FWA_max = 0;
@@ -176,7 +175,10 @@ for idxUEDensity = 1:length(lambda_UE)
                 end
                 mean_rate_dl = mean(rate_dl,2);
                 [cell_util, FWA_util] = computeUtility(params,mean_rate_dl);
-                if ((sum(cell_util) >= max_cell_util) && (sum(FWA_util) >= max_FWA_util))
+                obj = params.price_cell_per_bit*sum(cell_util) + params.price_FWA_per_bit*sum(FWA_util);
+                if (obj >= max_obj)
+                % if ((sum(cell_util) >= max_cell_util) && (sum(FWA_util) >= max_FWA_util))
+                    max_obj = obj;
                     max_cell_util = sum(cell_util);
                     max_FWA_util = sum(FWA_util);
                     K_FWA_max = K_FWA;
@@ -213,11 +215,12 @@ for idxUEDensity = 1:length(lambda_UE)
         numBS = size(params.locationsBS,1);
         result_string = strcat('/results_numFWA_',num2str(params.SC), 'SC_', num2str(numCPE_all),...
             'CPE_',num2str(lambda_BS(idxBSDensity)),...
-            'lambdaBS_',num2str(lambda_UE(idxUEDensity)),...
+            'lambdaBS_',num2str(lambda_SC(idxBSDensity)),...
+            'lambdaSC_',num2str(lambda_UE(idxUEDensity)),...
             'lambdaUE_', num2str(deployRange),'deployRange_',aID);
         recording_text_file_string = strcat(rateFolder,result_string,'.csv');
         fileID = fopen(recording_text_file_string,'w');
-        output_categories = ['lambdaBS,','numCPE,','lambdaUE,',...
+        output_categories = ['lambdaBS,','lambdaSC,','numCPE,','lambdaUE,',...
         'deployRange,','max_pow_fac,','max_FWA,','max_cell_util,','max_FWA_util\n'];
         fprintf(fileID,output_categories);
 
@@ -228,8 +231,8 @@ for idxUEDensity = 1:length(lambda_UE)
         % fprintf(fileID,formatSpec,lambda_BS(idxBSDensity),numCPE, ...
         % lambda_UE(idxUEDensity),p_fac,deployRange,mean_rate_FWA,mean_rate_cell);
 
-        formatSpec = '%d,%d,%d,%f,%d,%f,%.16f,%.16f\n';
-        fprintf(fileID,formatSpec,lambda_BS(idxBSDensity),numCPE_all, ...
+        formatSpec = '%d,%d,%d,%d,%f,%d,%f,%.16f,%.16f\n';
+        fprintf(fileID,formatSpec,lambda_BS(idxBSDensity),lambda_SC(idxBSDensity),numCPE_all, ...
         lambda_UE(idxUEDensity),deployRange,p_fac_max,K_FWA_max,max_cell_util,max_FWA_util);
         fclose(fileID);
     end
--- computeUtility.m
+++ computeUtility.m
@@ -5,5 +5,5 @@ K = params.numCPE + params.numUE;
 % FWA_util = params.price_FWA*sum(mean_rate_dl(1:K_FWA) > params.r_min_FWA);
 % cell_util = params.price_cell*sum(mean_rate_dl((1+K_FWA):K) > params.r_min_cell);
 FWA_util = max(min(1,(exp(10^(-8)*mean_rate_dl(1:K_FWA)) - exp(10^(-8)*params.r_min_FWA))/(exp(10^(-8)*params.r_max_FWA) - exp(10^(-8)*params.r_min_FWA))),0);
-cell_util = 1/(1+exp(-10^(-8)*(mean_rate_dl((1+K_FWA):K)-params.r_min_cell)));
+cell_util = 1./(1+exp(-10^(-8)*(mean_rate_dl((1+K_FWA):K)-params.r_min_cell)));
 end
